e5ac00732e66324fe6a38260349fd63ab539e6f9
geots <77642975+Geots@users.noreply.github.com>
2022-09-11 11:40:19 +0200

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,70 @@
+import socket
+import threading
+import os
+
+#clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Connection Data
+h_name = socket.gethostname()
+host = socket.gethostbyname(h_name)
+port = 55555
+
+print('Server info:')
+print('IP Address: ', host)
+print('Port: 55555')
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+        
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
